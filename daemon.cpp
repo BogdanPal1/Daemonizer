da1bad9ee1942be92895d1ef2c54ff99bf0e362e
Bogdan <BPalkin@aorti.ru>
2021-03-02 13:11:19 +0300

    Added definition of Daemon class

--- daemon.cpp
+++ daemon.cpp
@@ -1,6 +1,120 @@
 #include "daemon.h"
 
+Daemon::pDaemon Daemon::_daemon = nullptr;
+QString Daemon::_pidFile = "";
+
+Daemon::pDaemon Daemon::getInstance()
+{
+    if (_daemon == nullptr)
+        _daemon = pDaemon{new Daemon()};
+    return std::move(_daemon);
+}
+
 Daemon::Daemon()
 {
+    _pidFile = QString("/tmp/TestDaemon.pid");
+    openlog("Daemon test", LOG_PID | LOG_CONS, LOG_DAEMON);
+}
 
+void Daemon::Daemonize()
+{
+    pid_t pid, sid;
+    struct rlimit flim;
+    char str[10];
+
+    if (getppid() == 1) {
+        return;
+    }
+
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    sid = setsid();
+    if (sid < 0) {
+        logMessage("Can't set session ID!");
+        exit(EXIT_FAILURE);
+    }
+
+    getrlimit(RLIMIT_NOFILE, &flim);
+    for (int i = 0; i < flim.rlim_max; ++i) {
+        close(i);
+    }
+
+    int i = open("/dev/null", O_RDWR);
+    dup(i);
+    dup(i);
+
+    umask(0);
+
+    if (chdir("/") < 0) {
+        logMessage("Can't change directory to root!");
+        exit(EXIT_FAILURE);
+    }
+
+    int lFile = open(_pidFile.toStdString().c_str(), O_RDWR|O_CREAT, 0640);
+
+    if (lFile < 0) {
+        logMessage("Can't open PID file!");
+        exit(EXIT_FAILURE);
+    }
+
+    if (lockf(lFile, F_TLOCK, 0) < 0) {
+        exit(EXIT_SUCCESS);
+    }
+    sprintf(str, "%d\n", getpid());
+    write(lFile, str, strlen(str));
+
+    signalHandlerConfig();
+
+    logMessage("Daemon started!");
+}
+
+void Daemon::signalHandlerConfig()
+{
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTERM, termHandler);
+    signal(SIGINT, termHandler);
+    signal(SIGTSTP, pauseHandler);
+    signal(SIGCONT, resumeHandler);
+}
+
+void Daemon::termHandler(int sig)
+{
+    switch (sig) {
+        case SIGTERM:
+            logMessage("Terminate signal catched!");
+            closelog();
+            unlink(_pidFile.toStdString().c_str());
+            exit(EXIT_SUCCESS);
+            break;
+        case SIGINT:
+            logMessage("Interrupt signal catched!");
+            closelog();
+            unlink(_pidFile.toStdString().c_str());
+            exit(EXIT_SUCCESS);
+            break;
+    }
+}
+
+void Daemon::pauseHandler(int sig)
+{
+    logMessage("Stop signal catched!");
+}
+
+void Daemon::resumeHandler(int sig)
+{
+    logMessage("Continue signal catched!");
+}
+
+void Daemon::logMessage(const QString &message)
+{
+    syslog(LOG_INFO, "%s", message.toStdString().c_str());
 }
--- daemon.h
+++ daemon.h
@@ -1,11 +1,49 @@
 #ifndef DAEMON_H
 #define DAEMON_H
 
+#include <QString>
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/syslog.h>
+#include <sys/resource.h>
+#include <memory>
+
+
 
 class Daemon
 {
 public:
+    typedef std::unique_ptr<Daemon> pDaemon;
+
+public:
+
+    static pDaemon getInstance();
+    Daemon(Daemon& other) = delete;
+    Daemon& operator=(Daemon& other) = delete;
+
+    static void Daemonize();
+
+    static void termHandler(int sig);
+    static void pauseHandler(int sig);
+    static void resumeHandler(int sig);
+
+    static void logMessage(const QString& message);
+
+protected:
     Daemon();
+private:
+    Daemon(Daemon&& other) = default;
+    Daemon& operator=(Daemon&& other) = default;
+    static void signalHandlerConfig();
+private:
+
+    static pDaemon _daemon;
+    static QString _pidFile;
 };
 
 #endif // DAEMON_H
